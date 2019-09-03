c0cf109573f9ad2d0355ed7bb5af03acc0690b65
Pablolio88 <Pablolio88@yandex.ru>
2019-09-03 13:27:07 +0300

    set NTP using Invoke-command

--- NTP.ps1
+++ NTP.ps1
@@ -1,11 +1,7 @@
-$Servs= Get-Content -Path C:\Users\pavel.kuprin\Serv.txt
+$Servs=Get-Content -Path C:\Users\pavel.kuprin\Serv.txt
 $cred=Get-Credential
-foreach ($Serv in $Servs) {
-    $sess = New-PSSession -Credential $cred -ComputerName $Serv
-    Enter-PSSession $sess
-    w32tm /config /manualpeerlist:wdc-adc-01.dc.syncplicity.com /syncfromflags:manual 
-    Stop-Service w32time
-    Start-Service w32time
-    Exit-PSSession
-    Remove-PSSession $sess
+foreach($Serv in $Servs){
+    Invoke-Command -ComputerName $Serv -ScriptBlock { w32tm /config /manualpeerlist:wdc-adc-01.dc.syncplicity.com /syncfromflags:manual } -credential $cred
+    Invoke-Command -ComputerName $Serv -ScriptBlock { Stop-Service w32time } -credential $cred
+    Invoke-Command -ComputerName $Serv -ScriptBlock { Start-Service w32time } -credential $cred 
 }
