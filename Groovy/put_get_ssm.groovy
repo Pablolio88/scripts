b476ac589716ff4637f3a9830dea5430a99ba01e
pavel.kuprin <pavel.kuprin@syncplicity.com>
2021-02-09 17:26:12 +0300

    put_get_ssm updated to accept map as input parameter

--- Groovy/put_get_ssm.groovy
+++ Groovy/put_get_ssm.groovy
@@ -1,54 +1,57 @@
 // Looking for component and it's version in SSM parameter store
 
 def call(Map params = [:]) {
-    // Variables definition
-    // def awsCredentials  = params.get('awsCredentials')
-    // def awsRegion    = params.get('SSMRegion', 'us-east-1')
-    // def componentName   = params.get('componentName')
-    // def componentsVersion = params.get('componentsVersion')
-    def prefix = params.get("${env.AWS_REGION}/${env.AGO_PRODUCT}/${env.AGO_SERVICE}/${env.COMPONENT}/${env.VERSION}")
+    def prefix = params.get("${env.AWS_REGION}/${env.AGO_PRODUCT}/${env.AGO_SERVICE}")
+    def output_map = [:]
+    for (entry in params) {
+        def prefix = params.get("${prefix}/${entry.key}/${entry.value}")
+        try {
+            println "Looking for SSM parameter with ${prefix} in ${env.AWS_REGION}"
+            def parameter_name = sh returnStdout : true,
+                script: """aws ssm get-parameters \
+                    --names ${prefix} \
+                    --query 'Parameters[*].[Name]' \
+                    --output text
+                """
 
-    try {
-        println "Looking for SSM parameter with ${prefix} in ${env.AWS_REGION}"
-        def prefix = sh returnStdout : true,
-            script: """aws ssm get-parameters \
-                --names ${prefix} \
-                --query 'Parameters[*].[Name]' \
-                --output text
-            """
+            if (prefix == 'null' && entry.value != 'Current')) {
+                println "Creating parameter of ${prefix} with value of ${entry.value} in ${env.AWS_REGION}"
+                sh """aws ssm put-parameter \
+                    --name ${prefix} \
+                    --type "String" \
+                    --value ${entry.value} \
+                    --region ${env.AWS_REGION}
+                """
+                output_map.put("${prefix}", "${entry.value}")
+                } else 
 
-        if (prefix == 'null' && env.VERSION != 'Latest')) {
-            println "Creating parameter of ${prefix} with value of ${env.COMPONENT} in ${env.AWS_REGION}"
-            sh """aws ssm put-parameter \
-                --name ${prefix} \
-                --type "String" \
-                --value ${env.VERSION} \
-                --region ${env.AWS_REGION}                """
+            if (prefix != 'null' && entry.value != 'Current') {
+                println "Updating version of ${prefix} in ${env.AWS_REGION}"
+                    sh """aws ssm put-parameter \
+                    --name ${prefix} \
+                    --type "String" \
+                    --value ${entry.value} \
+                    --overwrite \
+                    --region ${env.AWS_REGION}
+                """
+                output_map.put("${prefix}", "${entry.value}")
             } else 
 
-        if (prefix != 'null' && env.VERSION != 'Latest') {
-            println "Updating version of ${prefix} in ${env.AWS_REGION}"
-                sh """aws ssm put-parameter \
-                --name ${prefix} \
-                --type "String" \
-                --value ${env.VERSION} \
-                --overwrite \
-                --region ${env.AWS_REGION}
-            """
-        } else 
-
-        if (prefix != 'null' && env.VERSION == 'Latest') {
-            println "Fetching version of ${prefix} in ${env.AWS_REGION}"
-            def env.VERSION = sh returnStdout : true,
-            script: """aws ssm get-parameters \
-                --names ${prefix} \
-                --query 'Parameters[*].[Value]' \
-                --output text
-            """
-        } else {
-            error "You are trying to get latest version of unexisting parameter. Check ${env.COMPONENT} version."
+            if (prefix != 'null' && entry.key == 'Current') {
+                println "Fetching version of ${prefix} in ${env.AWS_REGION}"
+                def parameter_value = sh returnStdout : true,
+                script: """aws ssm get-parameters \
+                    --names ${prefix} \
+                    --query 'Parameters[*].[Value]' \
+                    --output text
+                """
+                output_map.put(parameter_name, parameter_value)
+            } else {
+                error "You are trying to get Current version of unexisting parameter. Check ${entry.key} version."
+            }
+        } catch (e) {
+            throw e
         }
-    } catch (e) {
-        throw e
     }
+    return output_map
 }
